d2d3347620502c705eb3daf1589628870b329b3f
Satish Kumar17 <Satish_Kumar17@BHUKRK230580D.local>
2014-10-20 15:47:14 +0530

    Initial Commit

---
+++ UnitTestCaseUsingXCT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitTestCaseUsingXCT
+//
+//  Created by Satish Kumar17 on 10/20/14.
+//  Copyright (c) 2014 Satish Kumar17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UnitTestCaseUsingXCT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnitTestCaseUsingXCT
+//
+//  Created by Satish Kumar17 on 10/20/14.
+//  Copyright (c) 2014 Satish Kumar17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
